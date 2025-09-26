c73b65d028961aa9673fe2b9ca440f5dd8e18d64
CLEYTON DURANS DO NASCIMENTO <cleyton.nascimento@aluno.senac.rio>
2025-09-26 19:18:35 -0300

    Atualizar02

--- teste.py
+++ teste.py
@@ -1,12 +1,11 @@
 funcionariosParaCadastrar = [
-    {"nome": "Cleyton", "sobrenome": "Durans", "idade": 25}
-    {"nome": "Lucas", "sobrenome": "Santos", "idade": 25}
+    {"nome": "Cleyton", "sobrenome": "Durans", "idade": 25},
+    {"nome": "Lucas", "sobrenome": "Santos", "idade": 25},
     {"nome": "Luiz", "sobrenome": "Nascimento", "idade": 25}
 ]
 
-cadastrosParaEnviarParaOBanco = [ 
-    
-]​
+cadastrosParaEnviarParaOBanco = [
+]
 
 class Pessoa:
     def __init__(self, nome, sobrenome, idade):
@@ -15,5 +14,5 @@ class Pessoa:
         self.idade = idade
 
     def salvar(self):
-        print(f"Foi salvo o  usuário {self.nome} {self.nome} de idade {self.nome} foi salvo com sucesso!")
+        print(f"Foi salvo o  usuário {self.nome} {self.sobrenome} de idade {self.idade} foi salvo com sucesso!")
 
